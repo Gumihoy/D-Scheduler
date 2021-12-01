a6b425482997466a5b16867b78338bea877dc6ac
Kent <ouyangkongtong@kuaishou.com>
2021-12-01 19:22:17 +0800

    Init

---
+++ Scheduler-admin/src/main/java/com/gumihoy/scheduler/admin/SchedulerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gumihoy.scheduler.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerAdminApplication.class, args);
+    }
+
+}
---
+++ Scheduler-admin/src/test/java/com/gumihoy/scheduler/admin/SchedulerAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumihoy.scheduler.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
